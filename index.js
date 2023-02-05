b7f39bd082b41fd2873d7340d37acae5964cbfef
ChambersJarod <Jarod.Chambers-Genge@keyin.com>
2023-02-05 20:22:29 -0330

    added comments to index.js

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+//my requirements
 const http = require('http');
 const fs = require('fs');
 const port = 3000;
 const events = require('events');
 
+//starting my event emitter
 const event = new events.EventEmitter();
 
+//creating the function for my event emitter
 event.on("errorFunc",function(message){
     console.log(message)
 })
 
+//creating the server
 const server = http.createServer(function(req, res){
+    //making the path to my pages a variable
     let path = "./views/"
     console.log(req.url);
     res.writeHead(200, { 'Content-Type': 'text/html' })
+    //starting my switch
     switch(req.url) {
+        //labeling the case for the switch
         case '/':
             fs.readFile(path+'index.html', function(error, data) {
+                //an if else function to detect errors
                 if (error) {
                     event.emit("errorFunc","there was an error finding index.html")
                     res.writeHead(404)
@@ -27,6 +35,7 @@ const server = http.createServer(function(req, res){
                 res.end()
             })
             break;
+        //all future 'case' objects are the same as the first one considering layout
         case '/about':
             fs.readFile(path+'about.html', function(error, data) {
                 if (error) {
@@ -83,6 +92,7 @@ const server = http.createServer(function(req, res){
     } 
 })
 
+//console.logging the port the server is connected to and giving an error if it does not connect
 server.listen(port, function(error){
     if (error) {
         console.log('something went wrong', error)
